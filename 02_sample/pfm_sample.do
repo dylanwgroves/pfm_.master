68f47b70a1202e3d394ec254467f2c6456557d8d
beatricemontano <69838233+beatricemontano@users.noreply.github.com>
2020-08-20 15:01:34 +0200

    remove API key

---
+++ 02_sample/pfm_sample.do
@@ -0,0 +1,31 @@
+* villages sample
+
+clear all
+import delimited "/Volumes/Secomba/BeatriceMontano/Boxcryptor/Box Sync/08_PanganiFM/PanganiFM/2 - Data and Analysis/2 - Final Data/genmatch sample_2018.02.17_west.csv", encoding(ISO-8859-1)
+keep objectid region_cod region_nam district_c district_n ward_code ward_name vil_mtaa_c vil_mtaa_n shape_leng shape_area vtreat match q1_2_geopointlatitude q1_2_geopointlongitude q1_2_geopointaltitude q1_2_geopointaccuracy q4_0_geopointlatitude q4_0_geopointlongitude q4_0_geopointaltitude q4_0_geopointaccuracy
+rename region_cod region_c
+rename region_nam region_n
+rename ward_code ward_c
+rename ward_name ward_n
+rename vil_mtaa_c village_c
+rename vil_mtaa_n village_n
+rename vtreat treat
+gen sample = "ne"
+rename q1_2_geopointlatitude latitude
+rename q1_2_geopointlongitude longitude
+save "/Users/BeatriceMontano/Dropbox/Wellspring Tanzania Papers/wellspring_01_master/01_data/02_mid_data/ne_sample_villages.dta", replace
+
+use "/Users/BeatriceMontano/Dropbox/Wellspring Tanzania Papers/Wellspring Tanzania - Audio Screening/01 Data/pfm2_randomized_vills.dta", clear
+keep district_c district_n ward_c ward_n village_n village_c radius dist_road dist_pfm dist_town area region_n region_c length latitude longitude dist_road_cent dist_coast vill_dist_pfm vill_dist_road vill_dist_town villcent_lat villcent_long treat 
+order region_n region_c district_c district_n ward_c ward_n village_n village_c length area treat  latitude longitude radius dist_road dist_pfm dist_town dist_road_cent dist_coast vill_dist_pfm vill_dist_road vill_dist_town villcent_lat villcent_long
+rename length shape_leng
+rename area shape_area
+gen sample = "as"
+save "/Users/BeatriceMontano/Dropbox/Wellspring Tanzania Papers/wellspring_01_master/01_data/02_mid_data/as_sample_villages.dta", replace
+
+append using "/Users/BeatriceMontano/Dropbox/Wellspring Tanzania Papers/wellspring_01_master/01_data/02_mid_data/ne_sample_villages.dta"
+
+sort region_n district_n ward_c 
+order region_c region_n district_c district_n ward_c ward_n village_c village_n sample  treat
+
+export excel region_c region_n district_c district_n ward_c ward_n village_c village_n sample  treat latitude longitude using "/Users/BeatriceMontano/Dropbox/Wellspring Tanzania Papers/wellspring_01_master/01_data/03_final_data/01_sample/villages.xls", firstrow(variables) replace
--- 02_sample/pfm_sample_map.R
+++ 02_sample/pfm_sample_map.R
@@ -5,6 +5,7 @@
 # Libraries ---------------------------------------------------------------
 library(data.table)
 library(dismo)
+library(plyr)      
 library(dplyr)
 library(geojsonio)
 library(ggmap)
@@ -25,11 +26,7 @@ library(readstata13)
 rm(list=ls())
 
 #Set your API Key ---------------------------------------------------------
-# Dylan
-# ggmap::register_google(key = "AIzaSyAzh5EMvmLELIQXvFJhbmD9pCD4vM_XPXA")
-
-# Bea
-ggmap::register_google(key = "AIzaSyBB5SbUdwqScXZBcrHfI2EU2aQqpza4RuE")
+ggmap::register_google(key = "")
 
 # Load Data ---------------------------------------------------------------
 ne_sample <- read.csv("/Volumes/Secomba/BeatriceMontano/Boxcryptor/Box Sync/08_PanganiFM/PanganiFM/2 - Data and Analysis/2 - Final Data/genmatch sample_2018.02.17_west.csv")
@@ -37,19 +34,35 @@ as_sample <- read.dta13("/Users/BeatriceMontano/Dropbox/Wellspring Tanzania Pape
 
 vills <- readOGR(dsn = "/Users/BeatriceMontano/Dropbox/Wellspring Tanzania Papers/wellspring_01_master/01_data/01_raw_data/02_publicdata/01_GIS/01_villages/Tanzania/TZvillages.shp")
 
-# Adjust shapefile for ggplot2 --------------------------------------------
-vills <- spTransform(vills, CRS("+proj=longlat +datum=WGS84 +units=km"))
-projarea <- vills[vills@data$Region_Nam == "Tanga",]
+# clean shapefiles for ggmap --------------------------------------------
+#sp_vills <- spTransform(vills, CRS("+proj=longlat +datum=WGS84 +units=km"))
+sp.projarea <- vills[vills@data$Region_Nam == "Tanga",]
 
-ne_IDsample <- ne_sample$OBJECTID
-ne_villstreat <- projarea[projarea@data$OBJECTID %in% ne_IDsample[ne_sample$v.treat == 1],]
-ne_villstreat <- fortify(ne_villstreat)
-ne_villscontrol <- projarea[projarea@data$OBJECTID %in% ne_IDsample[ne_sample$v.treat == 0],]
-ne_villscontrol <- fortify(ne_villscontrol)
-ne_villspanganifm <- projarea[projarea@data$OBJECTID %in% ne_IDsample[ne_sample$OBJECTID == 14937 | ne_sample$OBJECTID == 14550],]
-ne_villspanganifm <- fortify(ne_villspanganifm)
+df.ne_IDsample <- ne_sample$OBJECTID
 
-# add code for as_sample areas -> hinges on pfm_as_sample.r
+# export lat and long for ne sample
+sp.ne_vills <- sp.projarea[sp.projarea@data$OBJECTID %in% df.ne_IDsample,]
+sp.ne_vills@data$id <- rownames(sp.ne_vills@data)
+sp.ne_vills@data <- join(sp.ne_vills@data, ne_sample, by="OBJECTID")
+df.ne_vills <- fortify(sp.ne_vills)
+df.ne_vills <- join(df.ne_vills,sp.ne_vills@data, by="id")
+write.csv(df.ne_vills, "/Users/BeatriceMontano/Dropbox/Wellspring Tanzania Papers/wellspring_01_master/01_data/02_mid_data/ne_sample_villages_latlong.csv")
+
+
+# NE sample
+sp.ne_villstreat <- sp.projarea[sp.projarea@data$OBJECTID %in% df.ne_IDsample[ne_sample$v.treat == 1],]
+df.ne_villstreat <- fortify(sp.ne_villstreat)
+sp.ne_villscontrol <- sp.projarea[sp.projarea@data$OBJECTID %in% df.ne_IDsample[ne_sample$v.treat == 0],]
+df.ne_villscontrol <- fortify(sp.ne_villscontrol)
+
+
+# AS sample
+merge <- merge(projarea, as_sample, by.x = c("District_N", "Ward_Name", "Vil_Mtaa_N"), by.y = c("district_n", "ward_n", "village_n"))
+merge <- merge[!is.na(merge$region_n),]
+as_sample_treat <- merge[merge$treat==1,]
+as_sample_treat <- fortify(sp.as_sample_treat)
+as_sample_control <- merge[merge$treat==0,]
+as_sample_control <- fortify(sp.as_sample_control)
 
 # Set colors ---------------------------------------------------------------
 col_as_T <- "#007AC1"
@@ -58,7 +71,7 @@ col_ne_T <- "#FDBB30"
 col_ne_C <- "#EF3B24"
 
 # Map ---------------------------------------------------------------------
-map <- ggmap(get_googlemap(center = c(lon = 38.8482, lat = -5.2565),
+map <- ggmap(get_googlemap(center = c(lon = 38.8482, lat = -5.2),
                          zoom = 10, scale = 2,
                          maptype ='terrain',
                          color = "bw")) 
@@ -78,9 +91,14 @@ map_treatment <- map +
 ggsave("/Users/BeatriceMontano/Dropbox/Wellspring Tanzania Papers/wellspring_01_master/02_outputs/pfm_sample_map_treatment.png", 
        plot = map_treatment, width = 12, height = 8, units = "in")
 
-map + 
-  geom_polygon(data=ne_villstreat, aes(x=long, y=lat, group=group), fill=col_ne_T, size=.2, color=col_as_T, alpha=0.5) +
-  geom_polygon(data=ne_villscontrol, aes(x=long, y=lat, group=group), fill=col_ne_C, size=.2, color=col_as_C, alpha=0.5) 
+map_treatment_areas <- map + 
+  geom_polygon(data=ne_villstreat, aes(x=long, y=lat, group=group), fill=col_ne_T, size=.2, color="orangered4", alpha=0.5) +
+  geom_polygon(data=ne_villscontrol, aes(x=long, y=lat, group=group), fill=col_ne_C, size=.2, color="orangered4", alpha=0.5) + 
+  geom_polygon(data=as_sample_treat, aes(x=long, y=lat, group=group), fill=col_as_T, size=.2, color=col_as_T, alpha=0.5) +
+  geom_polygon(data=as_sample_control, aes(x=long, y=lat, group=group), fill=col_as_C, size=.2, color=col_as_T, alpha=0.5) 
+ggsave("/Users/BeatriceMontano/Dropbox/Wellspring Tanzania Papers/wellspring_01_master/02_outputs/pfm_sample_map_treatment_areas.png", 
+       plot = map_treatment_areas, width = 12, height = 8, units = "in")
+
 
 
 
